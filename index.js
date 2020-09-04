ae5a6c0359f962375342e9c27e61e17a4e786927
masonh627 <69410129+masonh627@users.noreply.github.com>
2020-09-03 21:08:09 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const {Client, Attachment, MessageEmbed} = require('discord.js');
 
-const token = 'YOUR TOKEN HERE';
+const token = 'YOUR TOKEN';
 
-var prefix = ';'; //change this if you want a custom prefix
+var prefix = ';';
 
-String; streamers = new Array("");
+streamersname = [];
+streamersurl = [];
 
 client.on('ready', () => {
     console.log('This bot is online!')
@@ -14,46 +16,68 @@ client.on('ready', () => {
 client.on('message', msg=>{
 
     let args = msg.content.substring(prefix.length).split(" ");
-
+    
     if(msg.content.startsWith(prefix)){
         switch(args[0]){
-            case 'help':
-                msg.channel.send("The Prefix is " + prefix + " and you're my favorite ;D");
-            break;
-            case 'twitch':
-                if(args.length == 2){
-                    switch(args[1]){
-                        case args[1] = 'ozark':
-                            msg.reply("https://www.twitch.tv/ozark_r6");
-                        break;
-                        case args[1] = 'snips':
-                            msg.reply("https://www.twitch.tv/snips_r6");
-                        break;
-                        case args[1] = 'pags':
-                            msg.reply("https://twitch.tv/pagsidw");
-                        break;
-                        case args[1] = 'chakra':
-                            msg.reply("https://twitch.tv/1chakra1 ");
-                        break;
-                        case args[1] = 'godzy':
-                            msg.reply("https://www.twitch.tv/godzyfps");
-                        break;
-                        case args[1] = 'ay':
-                            msg.reply("https://www.twitch.tv/aypsychopomp")
-                        break;
-                    }
+            case 'adds':
+                if(streamersurl.indexOf(args[2]) == -1 && args[1] != 'undefined' && args[2] != 'undefined'){
+                    streamersname.add(args[1])
+                    streamersurl.add(args[2])
+                    msg.channel.send("streamer sucsessfully added!")
                 }else{
-                    msg.reply('You need to enter a channel name! \n' + 'ozark \n' + 'snips \n' + 'pags \n' + 'chakra \n' + 'godzy \n');
+                    msg.channel.send("An error has accoured do " + prefix + "addshelp if you need help using this command")
                 }
             break;
+            case 'streamers':
+                if(streamersurl.length == 0){
+                    msg.channel.send("Please add some streamers first!")
+                    return;
+                }
+                message = 'The currently added streamers are\n\n';
+                for(x = 0; x < streamersname.length; x++){
+                    message += streamersname[x] + "\n"
+                }
+                msg.channel.send(message + "\nto get all the urls of the streamers do " + prefix + "streamersurl to get a dm with a list of them");
+            break;
+            case 'streamersurl':
+                if(streamersurl.length == 0){
+                    msg.channel.send("Please add some streamers first!")
+                    return;
+                }
+                message = 'The curently added streamer urls are\n\n'
+                for(x = 0; x <streamersurl.length; x++){
+                    message += streamersurl[x] + "\n"
+                }
+                msg.author.send(message)
             case 'prefix':
                 if(args.length == 2){
+                    if(args[1].length > 2){
+                        msg.channel.send("Prefix to long!")
+                        return;
+                    }
                     prefix = args[1];
                     msg.channel.send('The Prefix was set to ' + prefix);
+                }else if(args.length == 1){
+                    msg.channel.send("the prefix is" + prefix)
+                }else{
+                    msg.channel.send("unknown use of command prefix")
                 }
             break;
             case 'cmds':
-                msg.channel.send("Current added commands \n" + prefix + "help \n" + "  This conmmand helps with basic stuff \n\n" + prefix + "twitch \n" + "  THIS COMMAND IS BEING WORKED ONE RN This command returns the link to the Twitch page of the person you put in \n\n" + prefix + "prefix + [anything] \n" + "  This command will change the prefix on the server");
+                msg.channel.send("Current added commands \n" + prefix + "adds\n" + "this command goes into a lot of depth for more help with it do " + prefix + "addshelp\n\n" + prefix + "prefix + [anything] \n" + "  This command will change the prefix on the server \n\n" + prefix + "RPS \n" + "  This command will return rather rock,paper or scissors");
+            break;
+            case 'help':
+                msg.channel.send("Current added commands \n" + prefix + "adds\n" + "this command goes into a lot of depth for more help with it do " + prefix + "addshelp\n\n" + prefix + "prefix + [anything] \n" + "  This command will change the prefix on the server \n\n" + prefix + "RPS \n" + "  This command will return rather rock,paper or scissors");
+            break;
+            case 'rps':
+                x = Math.random()
+                if(x<= 0.33){
+                    msg.reply("Rock");
+                }if (x<= 0.66 && x>= 0.34){
+                    msg.reply("Scissors")
+                } else {
+                    msg.reply("Papper")
+                }
             break;
         }
     }
