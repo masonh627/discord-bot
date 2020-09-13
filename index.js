b85e7631660cbff38da97c89a2aa289f3039e5ed
masonh627 <69410129+masonh627@users.noreply.github.com>
2020-09-12 19:55:54 -0500

    added the more optimized code to index.js
    old code defined the var embed wayyyy to much so made it so it only defines it once at the start of the command chain

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const {Client, Attachment, MessageEmbed} = require('discord.js');
 
-const token = 'YOUR TOKEN';
+const token = 'NzQxNzM5MDE1MTE5NzAwMDcx.Xy78Sg.zXyCTT5B0YXaPtMDPOTFuwbVfIw';
 
 const ver = '2.0.0!'
 
@@ -19,42 +19,44 @@ client.on('ready', () => {
 client.on('message', msg=>{
 
     let args = msg.content.substring(prefix.length).split(" ");
-    var embed = new Discord.MessageEmbed()
 
     if(msg.content.startsWith(prefix)){
+        var embed = new Discord.MessageEmbed()
         switch(args[0]){
             case 'adds':
-                console.log[0]
-                console.log[1]
-                console.log[2]
-                if(streamersurl.indexOf(args[2]) == -1 && args.length == 3){
-                    streamersname.push(args[1])
-                    streamersurl.push(args[2])
-                    var embed = new Discord.MessageEmbed()
-                    .setColor('#8e44ad')
-                    .setTitle("streamer sucsessfully added!")
-                    .setFooter("do " + prefix + "streamers for a list of streamers")
-                    .setThumbnail("https://blog.twitch.tv/assets/uploads/generic-email-header-1.jpg")
-                    msg.channel.send(embed)
+                if(streamersurl.indexOf(args[2]) == -1 && typeof(args[1]) !== 'undefined' && typeof(args[2]) !== 'undefined'){
+                    if(args[2].includes('.com')){
+                        streamersname.push(args[1])
+                        streamersurl.push(args[2])
+                        embed.setColor('#8e44ad')
+                        embed.setTitle("streamer sucsessfully added!")
+                        embed.setDescription("do " + prefix + "streamers for a list of streamers")
+                        embed.setThumbnail("https://blog.twitch.tv/assets/uploads/generic-email-header-1.jpg")
+                        msg.channel.send(embed)
+                    }else{
+                        embed.setTitle("Error")
+                        embed.setDescription("invalid URL")
+                        embed.setThumbnail("https://blog.twitch.tv/assets/uploads/generic-email-header-1.jpg")
+                        embed.setColor('#8e44ad')
+                        msg.channel.send(embed)
+                    }
                 }else{
-                    var embed = new Discord.MessageEmbed()
-                    .setColor('#8e44ad')
-                    .setTitle("Error")
-                    .addFields(
+                    embed.setColor('#8e44ad')
+                    embed.setTitle("Error")
+                    embed.addFields(
                         {name:"An error has occurred", value:"do " + prefix + "addshelp to get help with this command"}
                     )
-                    .setThumbnail("https://blog.twitch.tv/assets/uploads/generic-email-header-1.jpg")
+                    embed.setThumbnail("https://blog.twitch.tv/assets/uploads/generic-email-header-1.jpg")
                     msg.channel.send(embed)
                     return;
                 }
             break;
             case 'streamers':
                 if(streamersurl.length == 0){
-                    var embed = new Discord.MessageEmbed()
-                    .setColor('#8e44ad')
-                    .setTitle("Please add some streamers first!")
-                    .setFooter("do " + prefix + "addshelp for more help")
-                    .setThumbnail("https://blog.twitch.tv/assets/uploads/generic-email-header-1.jpg")
+                    embed.setColor('#8e44ad')
+                    embed.setTitle("Please add some streamers first!")
+                    embed.setFooter("do " + prefix + "addshelp for more help")
+                    embed.setThumbnail("https://blog.twitch.tv/assets/uploads/generic-email-header-1.jpg")
                     msg.channel.send(embed)
                     return;
                 }
@@ -62,22 +64,20 @@ client.on('message', msg=>{
                 for(x = 0; x < streamersname.length; x++){
                     message += x + ". " + streamersname[x] + "\n"
                 }
-                var embed = new Discord.MessageEmbed()
-                .setColor('#8e44ad')
-                .setTitle("The currently added streamers are\n")
-                .addFields(
+                embed.setColor('#8e44ad')
+                embed.setTitle("The currently added streamers are\n")
+                embed.addFields(
                     {name:"Streamers", value:message, inline:true}
                 )
-                .setFooter("to get all the urls of the streamers do " + prefix + "streamersurl to get a dm with a list of them\n")
-                .setThumbnail("https://blog.twitch.tv/assets/uploads/generic-email-header-1.jpg")
+                embed.setFooter("to get all the urls of the streamers do " + prefix + "streamersurl to get a dm with a list of them\n")
+                embed.setThumbnail("https://blog.twitch.tv/assets/uploads/generic-email-header-1.jpg")
                 msg.channel.send(embed);
             break;
             case 'streamersurl':
                 if(streamersurl.length == 0){
-                    var embed = new Discord.MessageEmbed()
-                    .setColor('#8e44ad')
-                    .setTitle("Please add some streamers first!")
-                    .setThumbnail("https://blog.twitch.tv/assets/uploads/generic-email-header-1.jpg")
+                    embed.setColor('#8e44ad')
+                    embed.setTitle("Please add some streamers first!")
+                    embed.setThumbnail("https://blog.twitch.tv/assets/uploads/generic-email-header-1.jpg")
                     msg.channel.send(embed)
                     return;
                 }
@@ -85,113 +85,102 @@ client.on('message', msg=>{
                 for(x = 0; x <streamersurl.length; x++){
                     message += x + ". " + streamersurl[x] + "\n"
                 }
-                var embed = new Discord.MessageEmbed()
-                .setColor('#8e44ad')
-                .setTitle("The currently added streamer urls are")
-                .addFields(
+                embed.setColor('#8e44ad')
+                embed.setTitle("The currently added streamer urls are")
+                embed.addFields(
                     {name:"Streamers", value:message, inline:true}
                 )
-                .setThumbnail("https://blog.twitch.tv/assets/uploads/generic-email-header-1.jpg")
+              embed.setThumbnail("https://blog.twitch.tv/assets/uploads/generic-email-header-1.jpg")
                 msg.author.send(embed)
             break;
             case 'prefix':
                 if(args.length == 2){
                     if(args[1].length > 2){
-                        var embed = new Discord.MessageEmbed()
-                        .setColor('#8e44ad')
-                        .setTitle("Prefix to long!")
-                        .setThumbnail("https://blog.twitch.tv/assets/uploads/generic-email-header-1.jpg")
+                        embed.setColor('#8e44ad')
+                        embed.setTitle("Prefix to long!")
+                        embed.setThumbnail("https://blog.twitch.tv/assets/uploads/generic-email-header-1.jpg")
                         msg.channel.send(embed)
                         return;
                     }
                     prefix = args[1];
-                    var embed = new Discord.MessageEmbed()
-                    .setColor('#8e44ad')
-                    .setTitle('The Prefix was set to ' + prefix)
-                    .setThumbnail("https://blog.twitch.tv/assets/uploads/generic-email-header-1.jpg")
+                    embed.setColor('#8e44ad')
+                    embed.setTitle('The Prefix was set to ' + prefix)
+                    embed.setThumbnail("https://blog.twitch.tv/assets/uploads/generic-email-header-1.jpg")
                     msg.channel.send(embed);
                 }else if(args.length == 1){
-                    var embed = new Discord.MessageEmbed()
-                    .setColor('#8e44ad')
-                    .setTitle("the prefix is" + prefix)
-                    .setThumbnail("https://blog.twitch.tv/assets/uploads/generic-email-header-1.jpg")
+                    embed.setColor('#8e44ad')
+                    embed.setTitle("the prefix is" + prefix)
+                    embed.setThumbnail("https://blog.twitch.tv/assets/uploads/generic-email-header-1.jpg")
                     msg.channel.send(embed)
-                }else{
-                    var embed = new Discord.MessageEmbed()
-                    .setColor('#8e44ad')
-                    .setTitle("unknown use of command prefix")
-                    .setThumbnail("https://blog.twitch.tv/assets/uploads/generic-email-header-1.jpg")
+                }else{ 
+                  embed.setColor('#8e44ad')
+                  embed.setTitle("unknown use of command prefix")
+                  embed.setThumbnail("https://blog.twitch.tv/assets/uploads/generic-email-header-1.jpg")
                     msg.channel.send(embed)
                 }
             break;
             case 'addshelp':
-                var embed = new Discord.MessageEmbed()
-                .setColor('#8e44ad')
-                .setTitle("Adds commands")
-                .addFields(
+                embed.setColor('#8e44ad')
+                embed.setTitle("Adds commands")
+                embed.addFields(
                     {name:"How to use adds:", value:prefix + "adds <streamername> <linktostreamer>"},
                     {name:"How to use streamers", value:prefix + "should return a list of all added streamers"},
                     {name:"How to use streamersurl", value:prefix + "should dm a list of all added streamer urls"}
                 )
-                .setThumbnail("https://blog.twitch.tv/assets/uploads/generic-email-header-1.jpg")
+                embed.setThumbnail("https://blog.twitch.tv/assets/uploads/generic-email-header-1.jpg")
                 msg.reply(embed);
             break;
             case 'help':
-                var embed = new Discord.MessageEmbed()
-                .setColor('#8e44ad')
-                .setTitle("Commands")
-                .addFields(
+                embed.setColor('#8e44ad')
+                embed.setTitle("Commands")
+                embed.addFields(
                     {name:prefix + "adds", value:"This command goes into alot more depth do " + prefix + "addshelp to get more info"},
                     {name:prefix + "prefix + [anything]", value:"this command will set the server prefix to what you say (the prefix can not be longer than 2 letter"},
                     {name:prefix + "bug", value:"This will give you the link to the form where you can report bugs/request features"},
                     {name:prefix + "source", value:"This will give you the link to the source code (the code will not always be up to date)"},
                 )
-                .setThumbnail("https://blog.twitch.tv/assets/uploads/generic-email-header-1.jpg")
+                embed.setThumbnail("https://blog.twitch.tv/assets/uploads/generic-email-header-1.jpg")
                 msg.channel.send(embed);
                 return;            
             break;
             case 'dev':
-                var embed = new Discord.MessageEmbed()
-                .setColor('#8e44ad')
-                .setTitle("This is just for the dev if you find this")
-                .setThumbnail("https://blog.twitch.tv/assets/uploads/generic-email-header-1.jpg")
-                .addFields(
+                embed.setColor('#8e44ad')
+                embed.setTitle("This is just for the dev if you find this")
+                embed.setThumbnail("https://blog.twitch.tv/assets/uploads/generic-email-header-1.jpg")
+                embed.addFields(
                     {name:"Version", value:ver},
                     {name:"uptime", value:client.uptime/1000}
                 )
                 msg.channel.send(embed);
             break;
             case 'bug':
-                var embed = new Discord.MessageEmbed()
-                .setColor('#8e44ad')
-                .setTitle("Bug report form")
-                .setDescription("The form for reporting bugs is in the title")
-                .setThumbnail("https://blog.twitch.tv/assets/uploads/generic-email-header-1.jpg")
-                .setURL("https://forms.gle/Wm3UWzhBMXGcMTVA7")
+                embed.setColor('#8e44ad')
+                embed.setTitle("Bug report form")
+                embed.setDescription("The form for reporting bugs is in the title")
+                embed.setThumbnail("https://blog.twitch.tv/assets/uploads/generic-email-header-1.jpg")
+                embed.setURL("https://forms.gle/Wm3UWzhBMXGcMTVA7")
                 msg.channel.send(embed);
             break;
             case 'source':
-                var embed = new Discord.MessageEmbed()
-                .setColor('#8e44ad')
-                .setTitle("Bot source code")
-                .setDescription("Source code for the bot")
-                .setThumbnail("https://blog.twitch.tv/assets/uploads/generic-email-header-1.jpg")
-                .setURL("https://github.com/masonh627/discord-bot")
+                embed.setColor('#8e44ad')
+                embed.setTitle("Bot source code")
+                embed.setDescription("Source code for the bot")
+                embed.setThumbnail("https://blog.twitch.tv/assets/uploads/generic-email-header-1.jpg")
+                embed.setURL("https://github.com/masonh627/discord-bot")
                 msg.channel.send(embed);
             break;
         }
     }
     if(msg.content.startsWith(';') && args[0] == 'elp'){
-        var embed = new Discord.MessageEmbed()
-        .setColor('#8e44ad')
-        .setTitle("Commands")
-        .addFields(
+        embed.setColor('#8e44ad')
+        embed.setTitle("Commands")
+        embed.addFields(
             {name:prefix + "adds", value:"This command goes into alot more depth do " + prefix + "addshelp to get more info"},
             {name:prefix + "prefix + [anything]", value:"this command will set the server prefix to what you say (the prefix can not be longer than 2 letter"},
             {name:prefix + "bug", value:"This will give you the link to the form where you can report bugs/request features"},
             {name:prefix + "source", value:"This will give you the link to the source code (the code will not always be up to date)"},
         )
-        .setThumbnail("https://blog.twitch.tv/assets/uploads/generic-email-header-1.jpg")
+        embed.setThumbnail("https://blog.twitch.tv/assets/uploads/generic-email-header-1.jpg")
         msg.channel.send(embed);           
     }
 })
